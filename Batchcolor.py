a9bb794292e3e8def8bebd4d8bda5886046c0ca6
gmbgu <gmb.gustavo@gmail.com>
2023-02-28 18:48:33 -0300

    Limpeza de código. Não testado.

--- Batchcolor.py
+++ Batchcolor.py
@@ -14,55 +14,61 @@ class Batchcolor:
 
     def __init__(self):
         # Carrega a configuração.
-        with open('secret.yaml') as yf:
-            config = yaml.load(yf, Loader=yaml.FullLoader)
+        try:
+            with open('secret.yaml') as yf:
+                config = yaml.load(yf, Loader=yaml.FullLoader)
+        except FileNotFoundError:
+            print("O arquivo de configuração (config.yaml) não foi encontrado.")
         self.api_key = config['API_KEY']
         self.url = config['URL']
         self.mask = config['MASK']
         self.bw_images = config['BW_IMAGES']
-        self.CONTRAST = config['CONTRAST']
+        self.contrast = config['CONTRAST']
         self.alpha = config['ALPHA']
         self.results_folder = config['RESULTS_FOLDER']
 
-# Carrega os arquivos a serem colorizados
-    def read_images(self):
+    # Carrega os arquivos a serem colorizados
+    def _read_images(self):
         directory_path = Path(self.bw_images)
         files = []
+        print(f'\nLendo imagens da pasta de origem...')
         for file_path in directory_path.glob('**/*'):
             if file_path.is_file():
                 rel_path = file_path.relative_to(directory_path)
                 files.append(str(rel_path))
-        print(f'\nImagens carregadas: {files[0:3]} e outras {len(files)-4}...\n')
+        print(f'Total de fotos encontradas: {len(files)}.\n({files[0:3]} e mais {len(files)-4}) \n')
         return files
 
     def colorize(self, files):
-        pass
+        lista_imagens = self._read_images()
+        confirmacao = input('Iniciar processo? (S/N)')
+        headers = {'api-key': self.api_key}
+        i = 1
+        for foto in lista_imagens:
+            print(f'Colorizando foto {i} de {len(lista_imagens)}: {foto}. Aguarde...')
+            with open(foto, 'rb') as f:
+                # Converte para base64
+                bw_pic = base64.b64encode(f.read()).decode('utf-8')
+            # Parametros do request
+            data = {'image': bw_pic, 'contrast': self.contrast, 'alpha': self.alpha}
+            # Envia o request para a API
+            response = requests.post(self.url, headers=headers, data=data)
+            # Recebe a resposta em JSON, e isola apenas a URL direta da imagem colorizada
+            result_url = json.loads(response.text)['output_url']
+            # Faz download da imagem obtida para a pasta de saída
+            response = requests.get(result_url)
+            filename = os.path.join(self.results_folder, os.path.basename(foto))
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            i += 1
+
+        print(f'\nAs imagens colorizadas foram salvas na pasta {self.results_folder}.')
 
 
 if __name__ == '__main__':
     # TODO: Add argparse
     batch = Batchcolor()
-    lista_imagens = batch.read_images()
+    # lista_imagens = batch.read_images()
 
-    i = 1
-    # TODO: Make this a funcion
-    for foto in lista_imagens:
-        print(f'Imagem {i} de {len(lista_imagens)}: {foto}. Aguarde...')
-        with open(foto, 'rb') as f:
-            # Converte para base64
-            bw_pic = base64.b64encode(f.read()).decode('utf-8')
-        # Parametros do request
-        data = {'image': bw_pic, 'contrast': CONTRAST, 'alpha': ALPHA}
-        # Envia o request para a API
-        response = requests.post(URL, headers=headers, data=data)
-        # Recebe a resposta em JSON, e isola apenas a URL direta da imagem colorizada
-        result_url = json.loads(response.text)['output_url']
-        # Faz download da imagem obtida para a pasta de saída
-        response = requests.get(result_url)
-        filename = os.path.join(RESULTS_FOLDER, os.path.basename(foto))
-        with open(filename, 'wb') as f:
-            f.write(response.content)
-        i += 1
 
-    print(f'\nAs imagens colorizadas foram salvas na pasta {RESULTS_FOLDER}.')
 
